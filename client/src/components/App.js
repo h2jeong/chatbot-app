9acd110ef25985014357b9c27ecd5af0f18cf68a
zoe <theonlyoneu@daum.net>
2020-06-26 17:17:02 +0900

    create html layout and page : login, register

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,35 @@
 import React from "react";
 import "./App.css";
-import { Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage/LandingPage";
 import LoginPage from "./views/LoginPage/LoginPage";
 import RegisterPage from "./views/RegisterPage/RegisterPage";
+import { Layout } from "antd";
+import NavBar from "./views/NavBar/NavBar";
+
+const { Footer, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" component={LandingPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/register" component={RegisterPage} />
-        </Switch>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <Layout className="layout">
+        <NavBar />
+        <Content style={{ padding: "30px 50px" }}>
+          <div className="site-layout-content">
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/register" component={RegisterPage} />
+            </Switch>
+          </div>
+        </Content>
+        <Footer
+          style={{ textAlign: "center", background: "#1890ff", color: "white" }}
+        >
+          Footer
+        </Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout, Menu } from "antd";
+
+import {
+  HomeOutlined,
+  UserAddOutlined,
+  LoginOutlined,
+  LogoutOutlined,
+  PoweroffOutlined
+} from "@ant-design/icons";
+
+const { Header } = Layout;
 
 function NavBar() {
-  return <div>NavBar</div>;
+  const [IsAuth, setIsAuth] = useState(false);
+
+  if (!IsAuth) {
+    return (
+      <Header style={{ padding: "0 50px", background: "white" }}>
+        <Menu mode="horizontal">
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <a href="/">Home</a>
+          </Menu.Item>
+          <Menu.Item key="login" icon={<PoweroffOutlined />}>
+            <a href="/login">Log In</a>
+          </Menu.Item>
+          <Menu.Item key="register" icon={<UserAddOutlined />}>
+            <a href="/register">Register</a>
+          </Menu.Item>
+        </Menu>
+      </Header>
+    );
+  } else {
+    return (
+      <Header style={{ padding: 0 }}>
+        <Menu mode="horizontal" selectedKeys={["home"]}>
+          <Menu.Item key="home" icon={<HomeOutlined />}>
+            <a href="/">Home</a>
+          </Menu.Item>
+          <Menu.Item key="logout" icon={<LogoutOutlined />}>
+            <a href="/logout">Log out</a>
+          </Menu.Item>
+        </Menu>
+      </Header>
+    );
+  }
 }
 
 export default NavBar;
